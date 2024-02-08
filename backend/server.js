9882c2ec431a7167d333e71469c43e6c8d934fcc
The-ENG-X <eng.ahmadalebrahim@gmail.com>
2024-02-08 15:27:48 +0100

    add all Routes

--- backend/server.js
+++ backend/server.js
@@ -94,7 +94,7 @@ app.get("/auth/google",
 app.get("/auth/google/callback",
   passport.authenticate("google", { failureRedirect: "/login" }),
   function(req, res) {
-    res.redirect('/');
+    res.redirect('http://localhost:3000/myspace');
   }
 );
 
@@ -125,4 +125,26 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+app.get("/userData", (req, res) => {
+  // Überprüfen, ob der Nutzer eingeloggt ist
+  if (req.isAuthenticated()) {
+    // req.user sollte das Nutzerobjekt aus der Deserialisierung enthalten
+    const userId = req.user.id;
+
+    db.get("SELECT id, username, name FROM users WHERE id = ?", [userId], (err, row) => {
+      if (err) {
+        console.error("Fehler beim Abrufen der Nutzerdaten:", err);
+        res.status(500).send("Interner Serverfehler");
+      } else if (row) {
+        res.json(row); // Sendet die Nutzerdaten als JSON-Response
+      } else {
+        res.status(404).send("Nutzer nicht gefunden");
+      }
+    });
+  } else {
+    // Wenn der Nutzer nicht eingeloggt ist, sende eine entsprechende Antwort
+    res.status(403).send("Nicht autorisiert");
+  }
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import GoogleSignInButton from './components/GoogleSignInButton';
 import React from 'react';
-import Home from "./Home"
-import About from "./About"
-import Loging from "./Loging"
+import Home from "./components/Home";
+import About from "./components/About"
+import Loging from "./components/Login"
+import MeinBereich from './components/MeinBereich';
 import { Routes, Route } from 'react-router-dom';
 
 
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/loging" element={<Loging />} />
+        <Route path="/myspace" element={<MeinBereich />} />
         {/* <GoogleSignInButton /> */}
       </Routes>
     </div>
